e3a1efa8a3157fb00c7ec7f4d2e5153a4c0d3f93
jnmk929 <amour1125.j.0609@gmail.com>
2021-03-03 15:58:00 +0900

    add #13 入力した文字が消える機能追加

--- static/scripts/typing.js
+++ static/scripts/typing.js
@@ -4,6 +4,7 @@ var btn_close = document.getElementById('close');
 
 const japanese_sentences = ['さあ張り切って頑張りましょう。'];
 const alphabet_sentences = ['saaharikitteganbarimashou.'];
+let idx = 0;
 
 btn_show.addEventListener('click', function() {
     dialog.showModal();
@@ -12,11 +13,15 @@ btn_show.addEventListener('click', function() {
 
 btn_close.addEventListener('click', function() {
     dialog.close();
+    idx = 0;
+    true_keys = '';
 }, false);
 
 function typingGame(){
-    printSentence(japanese_sentences[0],alphabet_sentences[0]);
-    show_keydown();
+    japanese_s = japanese_sentences[0];
+    alphabet_s = alphabet_sentences[0];
+    printSentence(japanese_s,alphabet_s);
+    show_keydown(alphabet_s);
 }
 
 function printSentence(japanese_s,alpabet_s){
@@ -24,22 +29,12 @@ function printSentence(japanese_s,alpabet_s){
     alphabet.innerHTML = alpabet_s;
 }
 
-function show_keydown(){
+function show_keydown(alphabet_s){
     document.onkeydown = function(e){
         let key = e.key;
-        input_key.innerHTML = key;
+        if(key == alphabet_s[idx]){
+            idx += 1;
+            alphabet.innerHTML = alphabet_s.slice(idx,);
+        }
     }
 }
-/*
-word = word.toLowerCase();
-if(e.keyCode == 190){
-    word = '.';
-}
-else if(e.keyCode == 188){
-    word = ',';
-}
-if(word == input_words[0][idx]){
-    true_key += word;
-    show_word.innerHTML = true_key;
-    idx += 1;
-}*/
