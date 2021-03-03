6af9b400be63c1eb6e6b5681547c524154107e79
yochimonji <yochimonji@gmail.com>
2021-03-03 12:29:21 +0900

    update #6 動画でテストできるようにした

--- doze_detection.py
+++ doze_detection.py
@@ -16,11 +16,11 @@ def compute_pos_faces_eyes(img):
     # OpenCVはBGR、フロントから送信されてくる画像がRGBの場合変更する
     # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_rects = face_cascade.detectMultiScale(gray, 1.1, 3)  # 検出されない場合空のタプル()が返却される
+    face_rects = face_cascade.detectMultiScale(gray, minNeighbors=2)  # 検出されない場合空のタプル()が返却される
     eye_rects = []
     for xf,yf,wf,hf in face_rects:
         roi = gray[yf:yf+hf, xf:xf+wf]
-        eye_rect = eye_cascade.detectMultiScale(roi)
+        eye_rect = eye_cascade.detectMultiScale(roi, minNeighbors=10)
         if eye_rect == ():
             continue
 
@@ -37,19 +37,38 @@ def compute_pos_faces_eyes(img):
 def check_status(img):
     return compute_pos_faces_eyes(img)
 
-
-# 下はテスト用に書いてるだけ
+# 以下テスト用プログラム
 if __name__ == '__main__':
-    img = cv2.imread('./data/image1.png')
-
-    # conpute_pos_faces_eyes関数のテスト
-    face_rects, eye_rects = compute_pos_faces_eyes(img)
-    print('face_rects :\n', face_rects)
-    print('eye_rects :\n', eye_rects)
-    for x, y, w, h in face_rects:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
-    for x, y, w, h in eye_rects:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
-    cv2.imshow('test', img)
-    cv2.waitKey()  # 何かキーを押したら終了、右上のXで終了しない
+    # 動画でテストする場合
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print('Camera is not opened')  # カメラが起動しない場合、VideoCaptureの引数を0~9で変えてみる
+    else:
+        while True:
+            ret, img = cap.read()
+            face_rects, eye_rects = compute_pos_faces_eyes(img)
+            print('face_rects :\n', face_rects)
+            print('eye_rects :\n', eye_rects)
+            for x, y, w, h in face_rects:
+                cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
+            for x, y, w, h in eye_rects:
+                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
+            cv2.imshow('video capture : Press "q", if you want to finish.', img)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                break
+    cap.release()
     cv2.destroyAllWindows()
+
+    # 画像でテストする場合
+    # img = cv2.imread('./data/image1.png')
+    # face_rects, eye_rects = compute_pos_faces_eyes(img)
+    # print('face_rects :\n', face_rects)
+    # print('eye_rects :\n', eye_rects)
+    # for x, y, w, h in face_rects:
+    #     cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
+    # for x, y, w, h in eye_rects:
+    #     cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
+    # cv2.imshow('test', img)
+    # cv2.waitKey()  # 何かキーを押したら終了、右上のXで終了しない
+    # cv2.destroyAllWindows()
