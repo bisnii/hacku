c1605c6225d857d0846c3930748ec944fab8e933
yochimonji <yochimonji@gmail.com>
2021-03-03 12:48:05 +0900

    add #6 活動状態を調べる関数を実装

--- doze_detection.py
+++ doze_detection.py
@@ -21,7 +21,7 @@ def compute_pos_faces_eyes(img):
     for xf,yf,wf,hf in face_rects:
         roi = gray[yf:yf+hf, xf:xf+wf]
         eye_rect = eye_cascade.detectMultiScale(roi, minNeighbors=10)
-        if eye_rect == ():
+        if len(eye_rect) == 0:
             continue
 
         # roi画像の座標から全体画像の座標にする
@@ -35,11 +35,20 @@ def compute_pos_faces_eyes(img):
     return face_rects, eye_rects
 
 def check_status(img):
-    return compute_pos_faces_eyes(img)
+    face_rects, eye_rects = compute_pos_faces_eyes(img)
+    if len(face_rects) == 0:
+        status = 'away'
+    else:
+        if len(eye_rects) == 0:
+            status = 'sleep'
+        else:
+            status = 'active'
+    return status
 
 # 以下テスト用プログラム
 if __name__ == '__main__':
     # 動画でテストする場合
+    # "q"キーでプログラム終了
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print('Camera is not opened')  # カメラが起動しない場合、VideoCaptureの引数を0~9で変えてみる
@@ -47,8 +56,8 @@ if __name__ == '__main__':
         while True:
             ret, img = cap.read()
             face_rects, eye_rects = compute_pos_faces_eyes(img)
-            print('face_rects :\n', face_rects)
-            print('eye_rects :\n', eye_rects)
+            # print('face_rects :\n', face_rects)
+            # print('eye_rects :\n', eye_rects)
             for x, y, w, h in face_rects:
                 cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
             for x, y, w, h in eye_rects:
@@ -57,18 +66,20 @@ if __name__ == '__main__':
             key = cv2.waitKey(1)
             if key & 0xFF == ord('q'):
                 break
+            print('status:', check_status(img))
     cap.release()
     cv2.destroyAllWindows()
 
     # 画像でテストする場合
-    # img = cv2.imread('./data/image1.png')
+    img = cv2.imread('./data/image1.png')
     # face_rects, eye_rects = compute_pos_faces_eyes(img)
-    # print('face_rects :\n', face_rects)
-    # print('eye_rects :\n', eye_rects)
+    # # print('face_rects :\n', face_rects)
+    # # print('eye_rects :\n', eye_rects)
     # for x, y, w, h in face_rects:
     #     cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
     # for x, y, w, h in eye_rects:
     #     cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
+    # print('status:', check_status(img))
     # cv2.imshow('test', img)
     # cv2.waitKey()  # 何かキーを押したら終了、右上のXで終了しない
     # cv2.destroyAllWindows()
