1f4bbf28591ee18f57c69e5f7888b808ae044236
jnmk929 <amour1125.j.0609@gmail.com>
2021-03-03 15:24:33 +0900

    add #13 入力文字の取得

--- static/scripts/typing.js
+++ static/scripts/typing.js
@@ -2,6 +2,8 @@ var dialog = document.querySelector('dialog');
 var btn_show = document.getElementById('show');
 var btn_close = document.getElementById('close');
 
+const japanese_sentences = ['さあ張り切って頑張りましょう。'];
+const alphabet_sentences = ['saaharikitteganbarimashou.'];
 
 btn_show.addEventListener('click', function() {
     dialog.showModal();
@@ -13,9 +15,31 @@ btn_close.addEventListener('click', function() {
 }, false);
 
 function typingGame(){
-    printSentence();
+    printSentence(japanese_sentences[0],alphabet_sentences[0]);
+    show_keydown();
 }
 
-function printSentence(){
-    sentence.innerHTML = 'さあ張り切って頑張りましょう。'
+function printSentence(japanese_s,alpabet_s){
+    japanese.innerHTML = japanese_s;
+    alphabet.innerHTML = alpabet_s;
 }
+
+function show_keydown(){
+    document.onkeydown = function(e){
+        let key = e.key;
+        input_key.innerHTML = key;
+    }
+}
+/*
+word = word.toLowerCase();
+if(e.keyCode == 190){
+    word = '.';
+}
+else if(e.keyCode == 188){
+    word = ',';
+}
+if(word == input_words[0][idx]){
+    true_key += word;
+    show_word.innerHTML = true_key;
+    idx += 1;
+}*/
