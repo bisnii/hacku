c46e2d1497ab1edba9b981f21be5eb85700dc85f
yochimonji <yochimonji@gmail.com>
2021-03-06 14:58:46 +0900

    add #21 居眠り検出テスト、1日の記録、ログインページを追加

--- app.py
+++ app.py
@@ -9,12 +9,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/get_status', methods=['POST'])
-# def get_status():
-#     img = request.files[]
-#     status = doze_detection.check_status(img)
-#     return jsonify({'status': status})
+@app.route('/get_status', methods=['POST'])
+def get_status():
+    img = request.files['']
+    status = doze_detection.check_status(img)
+    return jsonify({'status': status})
 
+@app.route('/detection_test')
+def detection_test():
+    return render_template('detection_test.html')
+
+@app.route('/activity_log')
+def activity_log():
+    return render_template('activity_log.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
