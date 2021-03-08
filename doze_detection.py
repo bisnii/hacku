698dc14952cf460dce559805281852072d3182d6
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-03-08 12:02:01 +0900

    Doze detection 居眠り検出を顔認識のみで行うように変更 (#24)
    * update #21 居眠り検知を顔検出のみで行うように変更
    
    * remove #21 目の検出用の重みを削除

--- doze_detection.py
+++ doze_detection.py
@@ -1,48 +1,26 @@
 import cv2
-import numpy as np
 
 
-def compute_pos_faces_eyes(img):
+def compute_pos_faces(img):
     if img is None:
         raise Exception('引数：imgがありません')
 
     face_cascade = cv2.CascadeClassifier('modules/haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('modules/haarcascade_eye.xml')
     if face_cascade.empty(): 
         raise IOError('Unable to load the face cascade classifier xml file') 
-    if eye_cascade.empty(): 
-        raise IOError('Unable to load the eye cascade classifier xml file') 
     
     # OpenCVはBGR、フロントから送信されてくる画像がRGBの場合変更する
     # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_rects = face_cascade.detectMultiScale(gray, minNeighbors=2)  # 検出されない場合空のタプル()が返却される
-    eye_rects = []
-    for xf,yf,wf,hf in face_rects:
-        roi = gray[yf:yf+hf, xf:xf+wf]
-        eye_rect = eye_cascade.detectMultiScale(roi, minNeighbors=10)
-        if len(eye_rect) == 0:
-            continue
-
-        # roi画像の座標から全体画像の座標にする
-        eye_rect += np.array((xf,yf,0,0))
-        eye_rects.append(eye_rect)
-
-    if eye_rects == []:
-        eye_rects = ()
-    else:
-        eye_rects = np.concatenate(eye_rects)
-    return face_rects, eye_rects
+    face_rects = face_cascade.detectMultiScale(gray, minNeighbors=3)  # 検出されない場合空のタプル()が返却される
+    return face_rects
 
 def check_status(img):
-    face_rects, eye_rects = compute_pos_faces_eyes(img)
+    face_rects = compute_pos_faces(img)
     if len(face_rects) == 0:
-        status = 'away'
+        status = 'sleep'
     else:
-        if len(eye_rects) == 0:
-            status = 'sleep'
-        else:
-            status = 'active'
+        status = 'active'
     return status
 
 # 以下テスト用プログラム
@@ -62,13 +40,9 @@ if __name__ == '__main__':
         else:
             while True:
                 ret, img = cap.read()
-                face_rects, eye_rects = compute_pos_faces_eyes(img)
-                # print('face_rects :\n', face_rects)
-                # print('eye_rects :\n', eye_rects)
+                face_rects = compute_pos_faces(img)
                 for x, y, w, h in face_rects:
                     cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
-                for x, y, w, h in eye_rects:
-                    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
                 cv2.imshow('video capture : Press "q", if you want to finish.', img)
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q'):
@@ -80,14 +54,10 @@ if __name__ == '__main__':
     # 画像でテストする場合
     else:
         img = cv2.imread(arg)
-        face_rects, eye_rects = compute_pos_faces_eyes(img)
-        # print('face_rects :\n', face_rects)
-        # print('eye_rects :\n', eye_rects)
+        face_rects = compute_pos_faces(img)
         for x, y, w, h in face_rects:
             cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 3)
-        for x, y, w, h in eye_rects:
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
-        print('status:', check_status(img))
         cv2.imshow('test', img)
+        print('status:', check_status(img))
         cv2.waitKey()  # 何かキーを押したら終了、右上のXで終了しない
         cv2.destroyAllWindows()
