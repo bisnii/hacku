a24f57fe51724ef447d5bd999f86998790501b1d
yochimonji <yochimonji@gmail.com>
2021-03-09 14:15:23 +0900

    update #21 保存を作業状態に変更

--- static/scripts/alarm.js
+++ static/scripts/alarm.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let timer = document.getElementById('timer')
     timer.innerHTML = "00:00";
     let dialog = document.querySelector('dialog');
-    let btn_show = document.getElementById('show');
+    let workStatusButton = document.getElementById('work_status_button');
     let timeLimit = 0;
     let timerID;
     let startFlag = false;
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let bgm = new Audio();
     let sound = new Audio();
     let status_queue = ['active', 'active']
+    resetAlarm();
+    setAlarm();
+    setBGM();
+
 
     // getUserMedia が使えないブラウザのとき
     if (typeof navigator.mediaDevices.getUserMedia !== 'function') {
@@ -23,36 +27,38 @@ document.addEventListener('DOMContentLoaded', function() {
         const videoArea = document.getElementById('video_area');  // 映像表示エリア
         videoArea.srcObject = stream
         setInterval(function() {
-            const canvas = document.getElementById('capture_image');  // キャンバス
-            const cct = canvas.getContext('2d');  // キャンバスの画像表示エリア
-            canvas.width  = videoArea.videoWidth;
-            canvas.height = videoArea.videoHeight;
-            cct.drawImage(videoArea, 0, 0);  // 動画をキャンバスに描画
+            if (workStatusButton.textContent === '作業中') {
+                const canvas = document.getElementById('capture_image');  // キャンバス
+                const cct = canvas.getContext('2d');  // キャンバスの画像表示エリア
+                canvas.width  = videoArea.videoWidth;
+                canvas.height = videoArea.videoHeight;
+                cct.drawImage(videoArea, 0, 0);  // 動画をキャンバスに描画
 
-            const base64 = canvas.toDataURL('image/png')
-            const formData = new FormData()
-            formData.append('img', base64)
-            $.ajax({
-                url: '/get_status',
-                type: 'POST',
-                data: formData,
-                contentType: false,
-                processData: false,
-                success: function(res) {
-                    status_queue.shift();
-                    status_queue.push(res['status']);
-                    if (!alarmFlag) {
-                        if (status_queue.toString() === 'active,active') {
-                            stop();
-                        } else if (status_queue.toString() === 'sleep,sleep') {
-                            start();
+                const base64 = canvas.toDataURL('image/png')
+                const formData = new FormData()
+                formData.append('img', base64)
+                $.ajax({
+                    url: '/get_status',
+                    type: 'POST',
+                    data: formData,
+                    contentType: false,
+                    processData: false,
+                    success: function(res) {
+                        status_queue.shift();
+                        status_queue.push(res['status']);
+                        if (!alarmFlag) {
+                            if (status_queue.toString() === 'active,active') {
+                                stop();
+                            } else if (status_queue.toString() === 'sleep,sleep') {
+                                start();
+                            }
                         }
+                    },
+                    error: function(errorThrown) {
+                        console.log('Error', errorThrown);
                     }
-                },
-                error: function(errorThrown) {
-                    console.log('Error', errorThrown);
-                }
-            })
+                })
+            }
         }, 3000)  // 本番時10000にする
     })
     .catch(err => {
@@ -60,17 +66,33 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
-    // 確定ボタンが押されたらアラーム時間表示
-    document.getElementById("check").addEventListener("click", function() {
-        sound.pause();
-        bgm.pause();
+    document.getElementById('alarmTime').addEventListener('change', function() {
         resetAlarm();
+    }, false);
+
+    document.getElementById('alarmMusic').addEventListener('change', function() {
+        setAlarm();
+    })
+
+    document.getElementById('bgm').addEventListener('change', function() {
+        setBGM();
+    }, false);
+
+    // 作業状態ボタンが押されたらアラーム時間表示
+    workStatusButton.addEventListener("click", function() {
+        if (workStatusButton.textContent === '作業中') {
+            workStatusButton.textContent = '退席中';
+            stop();
+        } else {
+            workStatusButton.textContent = '作業中';
+        }
     }, false);
 
     dialog.addEventListener('close', function() {
         alarmFlag = false;
-        stop()
+        stop();
     }, false);
+
 
     function resetAlarm() {
         let $alarmTime = document.getElementById("alarmTime").value;
@@ -96,15 +118,28 @@ document.addEventListener('DOMContentLoaded', function() {
             timeLimit = 3600000;
         }
     }
+
+    function setAlarm() {
+        let $alarmChoice = document.getElementById("alarmMusic").value;
+        sound.pause();
+        sound = new Audio("/static/alarm/sound-" + $alarmChoice + ".mp3");
+        sound.loop = true;
+    }
+
+    function setBGM() {
+        let $bgmChoice = document.getElementById("bgm").value;
+        bgm.pause();
+        if ($bgmChoice === 'none') {
+            bgm = new Audio();
+        } else {
+            bgm = new Audio("/static/bgm/bgm-" + $bgmChoice + ".mp3");
+        }
+        bgm.loop = true;
+    }
     
     function start() {
+        bgm.play();
         if ((! startFlag) && (timeLimit > 0)){
-            let $bgmChoice = document.getElementById("bgm").value;
-            let $alarmChoice = document.getElementById("alarmMusic").value;
-            bgm = new Audio("/static/bgm/bgm-" + $bgmChoice + ".mp3");
-            sound = new Audio("/static/alarm/sound-" + $alarmChoice + ".mp3");
-            bgm.loop = true;
-            bgm.play();
             startFlag = true;
             timerID = setInterval(time, 1000)
         }    
@@ -129,7 +164,6 @@ document.addEventListener('DOMContentLoaded', function() {
             clearInterval(timerID)
             alarmFlag = true;
             startFlag = false;
-            sound.loop = true;
             bgm.pause();
             sound.play();
             timer.innerHTML = "Wake Up!";
